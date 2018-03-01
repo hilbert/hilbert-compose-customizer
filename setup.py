1d955dd85b6c2302abea002bd8d78a4b97528978
Oleksandr Motsak <malex984@users.noreply.github.com>
2018-03-01 14:53:17 +0100

    Change of credits

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='nvidia-docker-compose',
+setup(name='hilbert-compose-customizer',
       version='0.1.3',
-      description='GPU enabled docker-compose wrapper',
-      url='https://github.com/eywalker/nvidia-docker-compose',
-      author='Edgar Y. Walker',
-      author_email='edgar.walker@gmail.com',
-      license='MIT',
+      description='GPU enabled docker-compose.override generator',
+      url='',
+      author='',
+      author_email='',
+      license='Apache 2',
       packages=[],
       install_requires=['pyyaml'],
-      scripts=['bin/nvidia-docker-compose']
+      scripts=['bin/hilbert-compose-customizer']
       )
